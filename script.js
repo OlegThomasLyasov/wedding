b1e01461a3f742f518ae2643fa0cf1d3c271ebb7
Oleg Lyasov <lov2103@yandex.ru>
2025-04-14 10:31:18 +0500

    update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function scrollMain() {
 function scrollMap() {
     document.getElementById("mapTitle").scrollIntoView({ block: "start", behavior: "smooth" });
 }
+
 // Инициализация карты после загрузки страницы
 ymaps.ready(init);
         
@@ -22,3 +23,61 @@ function init() {
     
     myMap.geoObjects.add(myPlacemark);
 }
+
+// форма
+document.addEventListener('DOMContentLoaded', ()=> {
+    const contactForm = document.getElementById('contact-form');
+    
+    if (contactForm) {
+        contactForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+            
+            // Получаем значения полей формы
+            const nameInput = document.querySelector('input[name="name"]');
+            const nameText = nameInput.value;
+            
+            const attendanceInput = document.querySelector('input[name="attendance"]');
+            const attendanceText = attendanceInput.value;
+            
+            // Отправка данных через Fetch API
+            fetch('https://formspree.io/f/manevaze', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: nameText,
+                    attendance: attendanceText
+                })
+            });
+        });
+    }
+});
+
+// таймер
+
+// Устанавливаем дату окончания
+const countdownDate = new Date("September 6, 2025 00:00:00").getTime();
+
+// Обновляем таймер каждую секунду
+const x = setInterval(function() {
+const now = new Date().getTime();
+const distance = countdownDate - now;
+
+// Вычисляем время
+const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+// Отображаем результат
+document.getElementById("countdown").innerHTML = 
+    days + "д " + hours + "ч " + minutes + "м " + seconds + "с ";
+
+// Если отсчет завершен, выводим сообщение
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Время вышло!";
+}
+}, 1000);
